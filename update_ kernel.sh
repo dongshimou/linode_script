76f6ec94c38914796f48df7d3ecb785d94dc02e2
dongshimou <dongshimou@gmail.com>
2019-03-14 18:16:49 +0800

    Update update_ kernel.sh

--- update_ kernel.sh
+++ update_ kernel.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+uname -r
+
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15/linux-image-4.15.0-041500-generic_4.15.0-041500.201802011154_amd64.deb
 
 dpkg -i linux-image-4.*.deb
